9e55da272b2994b02d00238349e03905b84979ac
wodud6967 <wodud6967@naver.com>
2024-03-27 11:34:10 +0900

    게시판만들기

---
+++ src/main/java/com/board/mapper/BoardMapper.java
@@ -0,0 +1,17 @@
+package com.board.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.board.domain.BoardVo;
+import com.board.menus.domain.MenuVo;
+
+@Mapper
+public interface BoardMapper {
+
+	List<BoardVo> getBoardList(MenuVo menuVo);
+
+	void insertBoard(BoardVo boardVo);
+
+}
